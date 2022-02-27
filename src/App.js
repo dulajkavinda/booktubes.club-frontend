bd381f81a34bd96693dd60db150ccf4ea81a7fec
Dulaj Kavinda <dulajkaavinda@gmail.com>
2022-02-27 22:05:14 +0530

    test and router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Dashboard/Dashboard.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Dashboard from "./Dashboard";
+
+let wrapper;
+beforeEach(() => {
+  wrapper = mount(<Dashboard />);
+});
+
+describe("dashboard basic tests", () => {
+  it("is dashboard renders", () => {
+    expect(wrapper.find("p")).toEqual({});
+  });
+});
