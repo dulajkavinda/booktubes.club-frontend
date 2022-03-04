51f6190b9ef613a769386ab0570b8cf7cbe83d6f
Dulaj Kavinda <dulajkaavinda@gmail.com>
2022-03-04 14:50:21 +0530

    dashboard card added

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "../../components/Club/Club";
 
 export default function Dashboard() {
   return (
     <div>
       <p id="dash-title">Dashboard</p>
+      <Card />
     </div>
   );
 }
