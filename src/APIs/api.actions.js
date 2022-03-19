dd2a85095c7637ff918e4dfb6d6a069aa849dbe5
Dulaj Kavinda <dulajkaavinda@gmail.com>
2022-03-19 23:37:19 +0530

    basic impl

--- src/APIs/api.actions.js
+++ src/APIs/api.actions.js
@@ -1,59 +1,69 @@
-import axios from 'axios';
-import { server } from './config';
+import axios from "axios";
+import { server } from "./config";
 
 //Users
 export const createUser = async (user) => {
-	const userExsits = await axios.get(
-		`${server.BACKEND_URL}/users/getUserById/${user.id}`,
-	);
-	if (userExsits.data.data.length === 0) {
-		const response = await axios.post(
-			`${server.BACKEND_URL}/users/signUp`,
-			user,
-		);
-		return response.data;
-	}
+  const userExsits = await axios.get(
+    `${server.BACKEND_URL}/users/getUserById/${user.id}`
+  );
+  if (userExsits.data.data.length === 0) {
+    const response = await axios.post(
+      `${server.BACKEND_URL}/users/signUp`,
+      user
+    );
+    return response.data;
+  }
 };
 
 export const getCurretReadingsData = async (userId, clubId) => {
-	const userData = await axios.get(`${server.BACKEND_URL}/users/getUserById/${userId}`);
+  const userData = await axios.get(
+    `${server.BACKEND_URL}/users/getUserById/${userId}`
+  );
 
-	const booksData = await axios.get(`${server.BACKEND_URL}/books/getBooks`);
+  const booksData = await axios.get(`${server.BACKEND_URL}/books/getBooks`);
 
-	let Readings = []
-	
-	booksData.data.data.forEach((book) => {
-		userData.data.data[0].current_readings.forEach((reading) => {
-			if ((book._id === reading.book_id) && (reading.club_id === clubId)) {
-				let obj ={
-					book: book,
-					progress: reading.percentage
-				}
-				Readings.push(obj);
-			}
-		});
-	})
+  let Readings = [];
 
-	return Readings;
-}
+  booksData.data.data.forEach((book) => {
+    userData.data.data[0].current_readings.forEach((reading) => {
+      if (book._id === reading.book_id && reading.club_id === clubId) {
+        let obj = {
+          book: book,
+          progress: reading.percentage,
+        };
+        Readings.push(obj);
+      }
+    });
+  });
+
+  return Readings;
+};
 
 //Clubs
 export const createClub = async (club) => {
-	const response = await axios.post(
-		`${server.BACKEND_URL}/clubs/createBookClub`,
-		club,
-	);
-	return response.data;
+  const response = await axios.post(
+    `${server.BACKEND_URL}/clubs/createBookClub`,
+    club
+  );
+  return response.data;
 };
 
 export const getClubs = async () => {
-	const response = await axios.get(`${server.BACKEND_URL}/clubs/getAllClubs`);
-	return response.data;
+  const response = await axios.get(`${server.BACKEND_URL}/clubs/getAllClubs`);
+  return response.data;
+};
+
+export const getUsers = async () => {
+  const response = await axios.get(`${server.BACKEND_URL}/users/getUsers`);
+  return response.data;
 };
 
 export const addMember = async (clubId, userId) => {
-	const response = await axios.put(`${server.BACKEND_URL}/clubs/updateMembers/${clubId}`, {
-		user: userId,
-	});
-	return response.data;
+  const response = await axios.put(
+    `${server.BACKEND_URL}/clubs/updateMembers/${clubId}`,
+    {
+      user: userId,
+    }
+  );
+  return response.data;
 };
---
+++ src/components/ClubProgress/ClubProgress.js
@@ -0,0 +1,135 @@
+import React from "react";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+
+import "./ClubProgress.scss";
+
+export default function ClubProgress({ users }) {
+  const data = {
+    members: [
+      {
+        userid: "001",
+        userName: "John Doe",
+        percentage: 24.0,
+      },
+      {
+        userid: "0zYPmgHgOHWWt0X12iudnT0bNZ03",
+        userName: "Emma MaCay",
+        percentage: 52.1,
+      },
+      {
+        userid: "003",
+        userName: "Beth Johns",
+        percentage: 73.0,
+      },
+      {
+        userid: "004",
+        userName: "John Doe",
+        percentage: 24.0,
+      },
+      {
+        userid: "005",
+        userName: "Emma MaCay",
+        percentage: 52.1,
+      },
+      {
+        userid: "006",
+        userName: "Beth Johns",
+        percentage: 73.0,
+      },
+      {
+        userid: "007",
+        userName: "John Doe",
+        percentage: 24.0,
+      },
+      {
+        userid: "008",
+        userName: "Emma MaCay",
+        percentage: 52.1,
+      },
+      {
+        userid: "009",
+        userName: "Beth Johns",
+        percentage: 73.0,
+      },
+      {
+        userid: "0012",
+        userName: "John Doe",
+        percentage: 24.0,
+      },
+      {
+        userid: "0013",
+        userName: "Emma MaCay",
+        percentage: 52.1,
+      },
+      {
+        userid: "0033",
+        userName: "Beth Johns",
+        percentage: 73.0,
+      },
+      {
+        userid: "0015",
+        userName: "John Doe",
+        percentage: 24.0,
+      },
+      {
+        userid: "0028",
+        userName: "Emma MaCay",
+        percentage: 52.1,
+      },
+      {
+        userid: "0037",
+        userName: "Beth Johns",
+        percentage: 73.0,
+      },
+      {
+        userid: "0019",
+        userName: "John Doe",
+        percentage: 24.0,
+      },
+      {
+        userid: "0020",
+        userName: "Emma MaCay",
+        percentage: 52.1,
+      },
+      {
+        userid: "0030",
+        userName: "Beth Johns",
+        percentage: 73.0,
+      },
+    ],
+  };
+
+  return (
+    <div className="member-progress-modal">
+      <Table sx={{ minWidth: 350 }} aria-label="Member progress table">
+        <TableBody>
+          {data.members.map((data) => (
+            <TableRow
+              key={data.userid}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {data.userid == localStorage.getItem("user")
+                  ? "Me"
+                  : data.userName}
+              </TableCell>
+              <TableCell>
+                <LinearProgress
+                  className="progress-bar"
+                  variant="determinate"
+                  value={data.percentage}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
