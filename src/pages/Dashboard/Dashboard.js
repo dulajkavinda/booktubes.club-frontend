d6cea393d17622367d6cd375ad8ea69df8a7f87f
kavvisha <k.dineth@gmail.com>
2022-04-09 19:46:53 +0530

    signin reset-pw integrate-showing-clubs updated-navbar

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useEffect, useState} from "react";
 import Navbar from "../../containers/NavBar/Navbar";
 import { Button } from "@chakra-ui/react";
 import Club from "../../components/Club/Club";
@@ -8,9 +8,32 @@ import Link from "next/link";
 import { useStoreState } from "easy-peasy";
 
 import clubdata from "../../data/clubs.json";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import {auth} from "../../../firebase";
+import { getClubs } from "../../APIs/api.actions";
 
 export default function Dashboard() {
   const user = useStoreState((state) => state.user);
+  const [loggedUser, loading,error] = useAuthState(auth);
+  const router = useRouter();
+  const [bookClubs,setBookClubs] = useState([]);
+
+  // check if user logged in or not 
+  useEffect(() => {
+    if (!loggedUser) {
+        router.push('/'); 
+    }
+  }, []);
+
+  useEffect( ()=>{
+    getClubs().then((res) => {
+      setBookClubs(res.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
 
   return (
     <div>
@@ -28,18 +51,38 @@ export default function Dashboard() {
 
         <Divider />
         <div className={styles.myclub}>
-          {clubdata.data.map((club) => {
+          {/* {clubdata.data.map((club) => {
             if (club.members.includes(user.uid)) {
               return <Club type="my" club={club} />;
             }
+          })} */}
+          {bookClubs.map((club) => {
+            console.log('club',club);
+            console.log('user id',loggedUser);
+
+            if(club.members.includes(loggedUser.uid)){
+              console.log('club members',club);
+              return <Club type="all" club={club} />
+            }
+            else{
+              console.log('no club members');
+            }
           })}
+
         </div>
         <span className={styles.title}>Explore More Clubs</span>
         <Divider />
         <div className={styles.myclub}>
-          {clubdata.data.map((club) => {
+
+          {/* {clubdata.data.map((club) => {
             return <Club type="all" club={club} />;
-          })}
+          })} */}
+          {
+            bookClubs.map((club) => {
+              return <Club type="all" club={club} />
+            })
+          }          
+
         </div>
       </div>
     </div>
--- src/pages/details/ClubDetails.js
+++ src/pages/details/ClubDetails.js
@@ -4,40 +4,48 @@ import { Button, ButtonGroup, Input } from "@chakra-ui/react";
 import { useStoreState } from "easy-peasy";
 import { useDisclosure } from "@chakra-ui/react";
 
+import Navbar from "../../containers/NavBar/Navbar";
+
 import AddPoll from "../create/AddPoll";
+
 export default function ClubDetails() {
   const selectedClub = useStoreState((state) => state.selectedClub);
   const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <div className={styles.main}>
-      <AddPoll isOpen={isOpen} onClose={onClose} />
-      <div className={styles.header}>
-        <div className={styles.other}>
-          <div className={styles.image}>
-            <span className={styles.title}>
-              {selectedClub ? selectedClub.club_name : ""}
-            </span>
-            <p>{selectedClub ? selectedClub.description : ""}</p>
-            {/* <p>Catogery : {selectedClub.category[0]}</p> */}
-          </div>
+  const user = useStoreState((state) => state.user);
 
-          <div className={styles.info}>
-            <img src={"https://picsum.photos/200/300"} />
+  return (
+    <div>
+      <Navbar username={user.displayName} />    
+      <div className={styles.main}>
+        <AddPoll isOpen={isOpen} onClose={onClose} />
+        <div className={styles.header}>
+          <div className={styles.other}>
+            <div className={styles.image}>
+              <span className={styles.title}>
+                {selectedClub ? selectedClub.club_name : ""}
+              </span>
+              <p>{selectedClub ? selectedClub.description : ""}</p>
+              {/* <p>Catogery : {selectedClub.category[0]}</p> */}
+            </div>
+
+            <div className={styles.info}>
+               <img src={ "https://picsum.photos/200/300" } />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.body}>
-        <Button onClick={onOpen} mt={4} colorScheme="blue">
-          Create Poll
-        </Button>
-      </div>
+        <div className={styles.body}>
+          <Button onClick={onOpen} mt={4} colorScheme="blue">
+            Create Poll
+          </Button>
+        </div>
 
-      <div className={styles.msg}>
-        <Input width={"80%"} placeholder="Basic usage" />
-        <Button onClick={onOpen} mt={4} colorScheme="blue">
-          Send Post
-        </Button>
+        <div className={styles.msg}>
+          <Input width={"80%"} placeholder="Basic usage" />
+          <Button onClick={onOpen} mt={4} colorScheme="blue">
+            Send Post
+          </Button>
+        </div>
       </div>
-    </div>
+    </div>  
   );
 }
